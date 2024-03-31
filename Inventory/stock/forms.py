90ffd8435543b34f284dff068d3dd77ee0620472
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-03-31 23:12:04 +0300

    updated forms

--- Inventory/stock/forms.py
+++ Inventory/stock/forms.py
@@ -4,12 +4,16 @@ from django.contrib.auth.forms import UserCreationForm
 from stock.models import Product, Order
 from .models import Product, Order
 
+
 class UserRegistry(UserCreationForm):
+
+    # Defines an email field in addition to default fields
     email = forms.EmailField()
 
     class Meta:
+        # Specifies the model to be used for the form
         model = User
-        fields = [
+        fields = [  # Defines the fields to be included in the form
             "username",
             "first_name",
             "last_name",
@@ -18,20 +22,27 @@ class UserRegistry(UserCreationForm):
             "password2",
         ]
 
+
 class ProductForm(forms.ModelForm):
     class Meta:
+        # Specifies the model to be used for the form
         model = Product
+
+        # Defines the fields to be included in the form
         fields = ["name", "category", "quantity",  "price", "description"]
+
 
 class OrderForm(forms.ModelForm):
     class Meta:
+        # Specifies the model to be used for the form
         model = Order
-        fields = [
+        fields = [  # Defines the fields to be included in the form
             'product', 'created_by', 'quantity', 'price', 'status'
         ]
         widgets = {
+            # Customizes the widget for 'product' field
             'product': forms.Select(attrs={'class': 'form-control'}),
             'created_by': forms.Select(attrs={'class': 'form-control'}),
             'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
             'price': forms.NumberInput(attrs={'class': 'form-control'}),
-            'status': forms.Select(attrs={'class': 'form-control'}),}
+            'status': forms.Select(attrs={'class': 'form-control'}), }
