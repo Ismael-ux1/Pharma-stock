56279064f57f1b5b709954602c29363b88b61b48
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-04-01 20:56:43 +0300

    updated views

--- Inventory/stock/models.py
+++ Inventory/stock/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db.models import Sum
-from django.core.exceptions import ValidationError
 import uuid
 
 
